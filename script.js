2e1b5ec64ab061ca87a41f7e78d52a5196a79b69
kamal4285 <kamalcb4285@gmail.com>
2026-02-22 15:21:02 +0600

    toggle tab bar

--- script.js
+++ script.js
@@ -1,25 +1,13 @@
-// // interview count
-// let interview = 0;
-// document.getElementById('interview-btn').addEventListener('click', function(){
-//     interview++;
-//     const totalInterview = document.getElementById('total-interview');
-//     totalInterview.innerText = interview;
-// })
+// btn toggle complete
+const totalTab = document.getElementById('card-container')
+const interviewTab = document.getElementById('default-tab');
+const rejectedTab = document.getElementById('default-tab');
 
-// // rejected count
-// let rejected = 0;
-// document.getElementById('rejected-btn').addEventListener('click', function(){
-//     rejected++;
-//     const totalrejected = document.getElementById('total-rejected');
-//     totalrejected.innerText = rejected;
-// })
+const totalTab2 = document.getElementById('all-counter-tab')
+const interviewTab2 = document.getElementById('interview-tab');
+const rejectedTab2 = document.getElementById('rejected-tab');
 
-// tab convert
 function show(id){
-    const totalTab = document.getElementById('card-container')
-    const interviewTab = document.getElementById('default-tab');
-    const rejectedTab = document.getElementById('default-tab');
-
     totalTab.classList.add('hidden');
     interviewTab.classList.add('hidden');
     rejectedTab.classList.add('hidden');
@@ -27,69 +15,98 @@ function show(id){
     const selected = document.getElementById(id);
     selected.classList.remove('hidden');
 }
+function toggleStyle(id){
+    totalTab2.classList.remove('btn-primary');
+    interviewTab2.classList.remove('btn-primary');
+    rejectedTab2.classList.remove('btn-primary');
 
-// button color alternation
-function addClass(){
-    const interviewTabColor = document.getElementById('interview-tab');
-    const rejectedTabColor = document.getElementById('rejected-tab');
+    totalTab2.classList.add('btn');
+    interviewTab2.classList.add('btn');
+    rejectedTab2.classList.add('btn');
 
-    // interviewTabColor.classList.add('btn-primary');
-    // rejectedTabColor.classList.add('btn-primary');
-
+    const selected = document.getElementById(id);
+    selected.classList.add('btn-primary');
 
 }
+
+
+
+// --------------
+let interviewList = [];
+let rejectedList = [];
 
 // all application count
-const application = document.getElementsByClassName('card');
+// const application = document.getElementsByClassName('card');
+// total applications
 const totalApplication = document.getElementById('total-application');
-for(let i = 0; i<application.length; i++){
-    totalApplication.innerText = i;
+// total interview
+const totalInterview = document.getElementById('total-interview');
+// total rejected
+const totalRejected = document.getElementById('total-rejected');
+
+const totalCard = document.getElementById('card-container');
+
+const mainContainer = document.querySelector('main');
+
+function calculateCount(){
+    totalApplication.innerText = totalCard.children.length;
+    totalInterview.innerText = interviewList.length;
+    totalRejected.innerText = rejectedList.length;
 }
+calculateCount();
+
+
+
+
+
+
+
+
 
 // delete application
-const deleteBtn = document.querySelectorAll('.delete-btn');
-deleteBtn.forEach(function(btn){
-    btn.addEventListener('click', function(){
-        console.log('hi')
-        btn.parentElement.remove();
-    })
-})
+// const deleteBtn = document.querySelectorAll('.delete-btn');
+// deleteBtn.forEach(function(btn){
+//     btn.addEventListener('click', function(){
+//         console.log('hi')
+//         btn.parentElement.remove();
+//     })
+// })
 
 
 // interview and reject counting
-let applicationStatus = null;
-let totalInterview = 0;
-let totalReject = 0;
+// let applicationStatus = null;
+// let totalInterview = 0;
+// let totalReject = 0;
 
-const totalI = document.getElementById('total-interview');
-const totalR = document.getElementById('total-rejected');
-function updateCount(){
-    totalI.innerText = totalInterview;
-    totalR.innerText = totalReject;
-}
+// const totalI = document.getElementById('total-interview');
+// const totalR = document.getElementById('total-rejected');
+// function updateCount(){
+//     totalI.innerText = totalInterview;
+//     totalR.innerText = totalReject;
+// }
 
 
-document.getElementsByClassName('interview-btn').addEventListener('click', function(){
-    console.log('hello world')
-    if(applicationStatus !== 'rejected'){
-        if(applicationStatus === 'intrview'){
-        totalReject--;
-       }
-       totalInterview++;
-       applicationStatus = 'rejected';
+// document.getElementsByClassName('interview-btn').addEventListener('click', function(){
+//     console.log('hello world')
+//     if(applicationStatus !== 'rejected'){
+//         if(applicationStatus === 'intrview'){
+//         totalReject--;
+//        }
+//        totalInterview++;
+//        applicationStatus = 'rejected';
        
-       updateCount();
-    }
-})
-document.getElementsByClassName('rejected-btn').addEventListener('click', function(){
-     if(applicationStatus !== 'interview'){
-       if(applicationStatus === 'rejected'){
-        totalInterview--;
-       }
-       totalReject++;
-       applicationStatus = 'interview';
+//        updateCount();
+//     }
+// })
+// document.getElementsByClassName('rejected-btn').addEventListener('click', function(){
+//      if(applicationStatus !== 'interview'){
+//        if(applicationStatus === 'rejected'){
+//         totalInterview--;
+//        }
+//        totalReject++;
+//        applicationStatus = 'interview';
        
-       updateCount();
-    }
-})
+//        updateCount();
+//     }
+// })
 
